ba1c50603ebc428fb0352cfc80215ee89972029b
shun6934 <>
2017-11-17 15:20:35 +0900

    javaファイル及びxmlファイル追加

---
+++ app/src/main/java/smartphoneapp_project/kanazawaapp_2017/Kaga1Activity.java
@@ -0,0 +1,7 @@
+package smartphoneapp_project.kanazawaapp_2017;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class Kaga1Activity extends AppCompatActivity {
+
+}
---
+++ app/src/main/java/smartphoneapp_project/kanazawaapp_2017/Washi1Activity.java
@@ -0,0 +1,7 @@
+package smartphoneapp_project.kanazawaapp_2017;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class Washi1Activity extends AppCompatActivity {
+
+}
