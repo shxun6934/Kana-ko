118335f3f4c572cb1a251155bb7ae7474e1ebe03
shun6934 <>
2017-11-25 17:56:59 +0900

    タイトル画面からマップ画面への画面遷移

--- app/src/main/java/smartphoneapp_project/kanazawaapp_2017/TitleActivity.java
+++ app/src/main/java/smartphoneapp_project/kanazawaapp_2017/TitleActivity.java
@@ -1,7 +1,27 @@
 package smartphoneapp_project.kanazawaapp_2017;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class TitleActivity extends AppCompatActivity {
+public class TitleActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_title);
+
+        Button toMap = (Button)findViewById(R.id.titleButton);
+        toMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(TitleActivity.this, MapActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
 
 }
