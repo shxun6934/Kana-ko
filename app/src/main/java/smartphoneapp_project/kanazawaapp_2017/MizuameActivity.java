1b717c3b085457826bd15fdba0718fe2d8062379
shun6934 <>
2017-12-03 18:06:05 +0900

    水飴画面　レイアウト

---
+++ app/src/main/java/smartphoneapp_project/kanazawaapp_2017/MizuameActivity.java
@@ -0,0 +1,14 @@
+package smartphoneapp_project.kanazawaapp_2017;
+
+import android.app.Activity;
+import android.app.Application;
+import android.os.Bundle;
+
+public class MizuameActivity  extends Activity {
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mizuame);
+    }
+}
