4905b20c4bf7e0b220368d4480106bb100ea945c
enomoto <b1644748@planet.kanazawa-it.ac.jp>
2017-12-07 19:15:18 +0900

    加速度センサーの追加

--- app/src/main/java/smartphoneapp_project/kanazawaapp_2017/Washi1Activity.java
+++ app/src/main/java/smartphoneapp_project/kanazawaapp_2017/Washi1Activity.java
@@ -1,7 +1,84 @@
 package smartphoneapp_project.kanazawaapp_2017;
 
+import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Handler;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-public class Washi1Activity extends AppCompatActivity {
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
+import static android.content.Context.SENSOR_SERVICE;
+
+public class Washi1Activity extends Activity implements Runnable, SensorEventListener{
+    SensorManager sm;
+    TextView tv;
+    Handler h;
+    float gx, gy, gz;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        LinearLayout ll = new LinearLayout(this);
+        setContentView(ll);
+
+        tv = new TextView(this);
+        ll.addView(tv);
+
+        h = new Handler();
+        h.postDelayed(this, 500);
+    }
+
+    @Override
+    public void run() {
+        tv.setText("X-axis : " + gx + "\n"
+                + "Y-axis : " + gy + "\n"
+                + "Z-axis : " + gz + "\n");
+        h.postDelayed(this, 500);
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sm = (SensorManager)getSystemService(SENSOR_SERVICE);
+        List<Sensor> sensors =
+                sm.getSensorList(Sensor.TYPE_ACCELEROMETER);
+        if (0 < sensors.size()) {
+            sm.registerListener(this, sensors.get(0),
+                    SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sm.unregisterListener(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        h.removeCallbacks(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        gx = event.values[0];
+        gy = event.values[1];
+        gz = event.values[2];
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
 }
+
--- app/src/main/java/smartphoneapp_project/kanazawaapp_2017/Washi1_CanvasView.java
+++ app/src/main/java/smartphoneapp_project/kanazawaapp_2017/Washi1_CanvasView.java
@@ -1,8 +1,56 @@
 package smartphoneapp_project.kanazawaapp_2017;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+import static smartphoneapp_project.kanazawaapp_2017.R.id.image;
+
 /**
  * Created by meiti on 2017/11/27.
  */
 
-public class Washi1_CanvasView {
+public class Washi1_CanvasView extends View {
+    private Paint paint;
+    private Bitmap bmp = null;
+    private float xpos;
+    private float ypos;
+    private float preX;
+    private float preY;
+
+    public Washi1_CanvasView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        paint = new Paint();
+
+        bmp = BitmapFactory.decodeResource(getResources(), R.drawable.kami2);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        // 背景、半透明
+        canvas.drawColor(Color.argb(125, 0, 0, 255));
+
+        canvas.drawBitmap(bmp, canvas.getWidth()/2+xpos, canvas.getHeight()/2+ypos, paint);
+    }
+
+    public void setPosition(float xp, float yp) {
+
+        float dT = 1.0f;
+        final float ax = -xp/5;
+        final float ay = yp/5;
+
+        xpos += preX*dT  + ax*dT*dT/2;
+        preX += ax*dT;
+
+        ypos += preY*dT  + ay*dT*dT/2;
+        preY += ay*dT;
+
+        // 再描画
+        invalidate();
+    }
 }
