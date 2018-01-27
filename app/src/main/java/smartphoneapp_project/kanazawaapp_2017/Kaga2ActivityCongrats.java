6b17f78285480a2d502109e6d4a60f9408208281
takahito1220 <b1606966@planet.kanazawa-it.ac.jp>
2018-01-27 16:50:36 +0900

    Kaa2のコードの書き換え

---
+++ app/src/main/java/smartphoneapp_project/kanazawaapp_2017/Kaga2ActivityCongrats.java
@@ -0,0 +1,85 @@
+package smartphoneapp_project.kanazawaapp_2017;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import static smartphoneapp_project.kanazawaapp_2017.Kaga1Activity.question;
+
+/**
+ * Created by Yuta on 2017/12/17.
+ */
+
+public class Kaga2ActivityCongrats extends Activity {
+//    @Override
+//    protected void onCreate(@Nullable Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_kaga2_congrats);
+//        ImageView seikaigazou0 = (ImageView) findViewById(R.id.seikai_gazou0);
+//        ImageView seikaigazou1 = (ImageView) findViewById(R.id.seikai_gazou1);
+//        ImageView seikaigazou2 = (ImageView) findViewById(R.id.seikai_gazou2);
+//
+//        Button tap = (Button) findViewById(R.id.tap);
+//
+//
+//        switch (question.get(0)) {
+//            case "きんじそう":
+//                seikaigazou0.setImageResource(R.drawable.kinnzisou_ira);
+//                break;
+//            case "かがふときゅうり":
+//                seikaigazou0.setImageResource(R.drawable.kyuuri_ira);
+//                break;
+//            case "かなざわいっぽんふとねぎ":
+//                seikaigazou0.setImageResource(R.drawable.negi_ira);
+//                break;
+//            case "かがれんこん":
+//                seikaigazou0.setImageResource(R.drawable.rennkonn_ira);
+//                break;
+//        }
+//
+//        switch (question.get(1)) {
+//            case "きんじそう":
+//                seikaigazou1.setImageResource(R.drawable.kinnzisou_ira);
+//                break;
+//            case "かがふときゅうり":
+//                seikaigazou1.setImageResource(R.drawable.kyuuri_ira);
+//                break;
+//            case "かなざわいっぽんふとねぎ":
+//                seikaigazou1.setImageResource(R.drawable.negi_ira);
+//                break;
+//            case "かがれんこん":
+//                seikaigazou1.setImageResource(R.drawable.rennkonn_ira);
+//                break;
+//        }
+//
+//        switch (question.get(2)) {
+//            case "きんじそう":
+//                seikaigazou2.setImageResource(R.drawable.kinnzisou_ira);
+//                break;
+//            case "かがふときゅうり":
+//                seikaigazou2.setImageResource(R.drawable.kyuuri_ira);
+//                break;
+//            case "かなざわいっぽんふとねぎ":
+//                seikaigazou2.setImageResource(R.drawable.negi_ira);
+//                break;
+//            case "かがれんこん":
+//                seikaigazou2.setImageResource(R.drawable.rennkonn_ira);
+//                break;
+//        }
+//        tap.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent1 = new Intent(Kaga2ActivityCongrats.this, Kaga1Activity.class);
+//                startActivity(intent1);
+//            }
+//        });
+//
+//    }
+
+
+}
