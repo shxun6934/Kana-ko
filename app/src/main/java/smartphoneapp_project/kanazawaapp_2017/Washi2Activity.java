c06d274f524af7bc4d024219c171e90629febf48
daichi77 <b1738164@planet.kanazawa-it.ac.jp>
2018-01-27 22:08:33 +0900

    変数の変更などの細かい修正

--- app/src/main/java/smartphoneapp_project/kanazawaapp_2017/Washi2Activity.java
+++ app/src/main/java/smartphoneapp_project/kanazawaapp_2017/Washi2Activity.java
@@ -14,8 +14,6 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 
-import static smartphoneapp_project.kanazawaapp_2017.R.drawable.kuroba;
-
 public class Washi2Activity extends Activity  implements View.OnTouchListener {
     private ImageView kurobaView;
     private ImageView momijiView;
@@ -23,21 +21,21 @@ public class Washi2Activity extends Activity  implements View.OnTouchListener {
     private Rect rect = new Rect();
     private ImageView momijishadow;
     private ImageView otibashadow;
-    private int oldX = 0;
-    private int oldY = 0;
-    int count = 0;
-    int x = 0;
-    int y = 0;
-    Button button;
-    ViewGroup.MarginLayoutParams startmarginkuroba;
-    ViewGroup.MarginLayoutParams startmarginmomiji;
-    ViewGroup.MarginLayoutParams startmarginotiba;
+    private int imageoldX = 0;//ドラッグ前の画像のx座標を保持する変数
+    private int imageoldY = 0;//ドラッグ前の画像のy座標を保持する変数
+    private int count = 0;
+    private int x = 0;
+    private int y = 0;
+    private Button button;
+    private ViewGroup.MarginLayoutParams startmarginkuroba;
+    private ViewGroup.MarginLayoutParams startmarginmomiji;
+    private ViewGroup.MarginLayoutParams startmarginotiba;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_washi2);
         Resources r = getResources();
-        Bitmap kurobaBmp = BitmapFactory.decodeResource(r, kuroba);
+        Bitmap kurobaBmp = BitmapFactory.decodeResource(r,R.drawable.kuroba);
         Bitmap momijiBmp = BitmapFactory.decodeResource(r, R.drawable.momiji1);
         Bitmap otibaBmp = BitmapFactory.decodeResource(r, R.drawable.momiji2);
         Bitmap momijikage = BitmapFactory.decodeResource(r, R.drawable.momiji1kage);
@@ -46,7 +44,7 @@ public class Washi2Activity extends Activity  implements View.OnTouchListener {
         kurobaView = (ImageView) findViewById(R.id.ImageView1);
         momijiView = (ImageView) findViewById(R.id.ImageView2);
         otibaView = (ImageView) findViewById(R.id.ImageView3);
-        momijishadow = (ImageView) findViewById(R.id.imageView2S);
+        momijishadow = (ImageView) findViewById(R.id.ImageView2S);
         momijishadow.setImageBitmap(momijikage);
         otibashadow = (ImageView) findViewById(R.id.ImageView3S);
         otibashadow.setImageBitmap(otibakage);
@@ -71,17 +69,17 @@ public class Washi2Activity extends Activity  implements View.OnTouchListener {
                 y = (int) event.getRawY();
                 if (event.getAction() == MotionEvent.ACTION_MOVE) {
                     //viewの位置を計算
-                    int diffX = kurobaView.getLeft() + (x - oldX);
-                    int diffY = kurobaView.getTop() + (y - oldY);
+                    int diffX = kurobaView.getLeft() + (x - imageoldX);
+                    int diffY = kurobaView.getTop() + (y - imageoldY);
                     //viewの移動
                     kurobaView.layout(diffX, diffY, diffX + kurobaView.getWidth(),
                             diffY + kurobaView.getHeight());
                     //今回のタッチ位置を保持
-                    oldX = x;
-                    oldY = y;
+                    imageoldX = x;
+                    imageoldY = y;
                 } else if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                    oldX = x;
-                    oldY = y;
+                    imageoldX = x;
+                    imageoldY = y;
                 } else if (event.getAction() == MotionEvent.ACTION_UP) {
                     kurobaView.setLayoutParams(startmarginkuroba);
                 }
@@ -94,17 +92,17 @@ public class Washi2Activity extends Activity  implements View.OnTouchListener {
                 y = (int) event.getRawY();
                 if (event.getAction() == MotionEvent.ACTION_MOVE) {
                     //viewの位置を計算
-                    int diffX = momijiView.getLeft() + (x - oldX);
-                    int diffY = momijiView.getTop() + (y - oldY);
+                    int diffX = momijiView.getLeft() + (x - imageoldX);
+                    int diffY = momijiView.getTop() + (y - imageoldY);
                     //viewの移動
                     momijiView.layout(diffX, diffY, diffX + momijiView.getWidth()
                             , diffY + momijiView.getHeight());
                     //今回のviewの位置を保持
-                    oldX = x;
-                    oldY = y;
+                    imageoldX = x;
+                    imageoldY = y;
                 } else if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                    oldX = x;
-                    oldY = y;
+                    imageoldX = x;
+                    imageoldY = y;
                 } else if (event.getAction() == MotionEvent.ACTION_UP) {
                     momijishadow.getHitRect(rect);
                     if (rect.contains(x, y)) {
@@ -132,17 +130,17 @@ public class Washi2Activity extends Activity  implements View.OnTouchListener {
                 y = (int) event.getRawY();
                 if (event.getAction() == MotionEvent.ACTION_MOVE) {
                     //viewの位置を計算
-                    int diffX = otibaView.getLeft() + (x - oldX);
-                    int diffY = otibaView.getTop() + (y - oldY);
+                    int diffX = otibaView.getLeft() + (x - imageoldX);
+                    int diffY = otibaView.getTop() + (y - imageoldY);
                     //viewの移動
                     otibaView.layout(diffX, diffY, diffX + otibaView.getWidth()
                             , diffY + otibaView.getHeight());
                     //今回のviewの位置を保持
-                    oldX = x;
-                    oldY = y;
+                    imageoldX = x;
+                    imageoldY = y;
                 } else if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                    oldX = x;
-                    oldY = y;
+                    imageoldX = x;
+                    imageoldY = y;
                 } else if (event.getAction() == MotionEvent.ACTION_UP) {
                     otibashadow.getHitRect(rect);
                     if (rect.contains(x, y)) {
