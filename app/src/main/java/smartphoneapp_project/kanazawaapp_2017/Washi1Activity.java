c0edd485fbba26f2b2b7c0643443f1c16b5427f9
enomoto <b1644748@planet.kanazawa-it.ac.jp>
2018-01-29 18:26:16 +0900

    加速度センサーシュウセイ

--- app/src/main/java/smartphoneapp_project/kanazawaapp_2017/Washi1Activity.java
+++ app/src/main/java/smartphoneapp_project/kanazawaapp_2017/Washi1Activity.java
@@ -1,47 +1,30 @@
 package smartphoneapp_project.kanazawaapp_2017;
 
 import android.app.Activity;
-import android.hardware.SensorManager;
-import android.os.Bundle;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
-import android.widget.ImageView;
-import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Point;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.view.Display;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.FrameLayout;
 import android.widget.ImageView;
-
-import java.util.List;
-
+import android.widget.Toast;
 
 
-public class Washi1Activity extends Activity  {
+public class Washi1Activity extends Activity implements SensorEventListener {
 
     private Button button_segue;
 
-/*
-    public  void  onClick(View v){
-
-        Intent intent = new Intent(Washi1Activity.this,Washi.class);
-        startActivity(intent);
-    }
-  */
-
-
+//    public  void  onClick(View v){
+//
+//        Intent intent = new Intent(Washi1Activity.this,Washi.class);
+//        startActivity(intent);
+//    }
 
 
     private static final String LOG_TAG = "WASHI1";
@@ -56,19 +39,15 @@ public class Washi1Activity extends Activity  {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_washi1);
-        Button button = (Button)findViewById(R.id.test);
+        Button button = (Button) findViewById(R.id.test);
 
-        button.setOnClickListener(new View.OnClickListener(){
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Washi1Activity.this, Washi.class);
                 startActivity(intent);
             }
         });
-
-
-
-
 
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
@@ -81,6 +60,10 @@ public class Washi1Activity extends Activity  {
         washiView.setScaleX(2);
         washiView.setScaleY(2);
         jayroMovableLayout.addView(washiView, 16.0f);
+
+        SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        Sensor accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_GAME);
     }
 
     @Override
@@ -95,5 +78,39 @@ public class Washi1Activity extends Activity  {
         if (unregister != null) {
             unregister.unregister();
         }
+
+        sensorManager.unregisterListener(this);
+    }
+
+    private int count;
+    private int shaken;
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() != Sensor.TYPE_ACCELEROMETER) {
+            return;
+        }
+
+        count += Math.abs(event.values[0]);
+        count += Math.abs(event.values[1]);
+
+        if (count > 500) {
+            Toast.makeText(this, "ふって！", Toast.LENGTH_SHORT).show();
+            Log.d(LOG_TAG, count + "");
+            shaken++;
+
+            if (shaken == 3) {
+                Toast.makeText(this, "かんせい！！", Toast.LENGTH_SHORT).show();
+                shaken = 0;
+                Intent intent = new Intent(Washi1Activity.this, Washi2Activity.class);
+                startActivity(intent);
+            }
+            count = 0;
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
