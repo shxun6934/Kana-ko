9d45c8f04eb0a2c575b6884eecc4176d10ec8416
小幡 十矛 <obata28@vpn166.vpn.kanazawa-it.ac.jp>
2018-02-02 16:47:37 +0900

    SharedPreferencesで値の受け渡しと画像の表示

--- app/src/main/java/smartphoneapp_project/kanazawaapp_2017/MapActivity.java
+++ app/src/main/java/smartphoneapp_project/kanazawaapp_2017/MapActivity.java
@@ -1,22 +1,49 @@
-        package smartphoneapp_project.kanazawaapp_2017;
+package smartphoneapp_project.kanazawaapp_2017;
 
-        import android.content.DialogInterface;
-        import android.content.Intent;
-        import android.os.Build;
-        import android.os.Bundle;
-        import android.support.annotation.RequiresApi;
-        import android.support.v7.app.AlertDialog;
-        import android.support.v7.app.AppCompatActivity;
-        import android.view.View;
-        import android.widget.Button;
-        import smartphoneapp_project.kanazawaapp_2017.Event.EventActivity;
-        import smartphoneapp_project.kanazawaapp_2017.Zukan.KagayasaiZukan1Activity;
+import android.app.assist.AssistStructure;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import smartphoneapp_project.kanazawaapp_2017.Event.EventActivity;
+import smartphoneapp_project.kanazawaapp_2017.Zukan.KagayasaiZukan1Activity;
 
 public class MapActivity extends AppCompatActivity {
+
+
+    //TODO:Viewの表示・非表示がうまくいかずにエラー
+    View view = findViewById(R.id.imageView5);
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        SharedPreferences data = getSharedPreferences("addKey", Context.MODE_PRIVATE);
+
+        //int datasave = data.getInt("StatusSava", 0);//データがない際に初期化する値：0
+
+        // "input"から読み出す、何もないときは"Nothing"を返す
+        String str = data.getString("StatusSave", "Nothing");
+
+        if (str.equals("WashiEasy")) {//追加した値：WashiEasy があったら、画像を表示
+            view.setVisibility(View.VISIBLE);
+        } else {
+            findViewById(R.id.imageView5).setVisibility(View.GONE);
+        }
+//        if(!str.equals("Nothing")) {
+//            textRead.setText(str);
+//        }
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
         /*Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);*/
@@ -25,7 +52,7 @@ public class MapActivity extends AppCompatActivity {
             @Override
             public void onClick(final View v) {//押下時の処理
                 //確認ダイアログの作成
-                AlertDialog.Builder alertDialog = new AlertDialog.Builder(MapActivity.this,R.style.MyAlertDialogStyle);
+                AlertDialog.Builder alertDialog = new AlertDialog.Builder(MapActivity.this, R.style.MyAlertDialogStyle);
                 alertDialog.setTitle("間違い探し(仮)");
                 alertDialog.setMessage("このあそびをやってみる？？");
                 alertDialog.setPositiveButton("　　　 すすむ　　　 ", new DialogInterface.OnClickListener() {
@@ -34,7 +61,7 @@ public class MapActivity extends AppCompatActivity {
 
                         // TODO:すすむボタンが押されたときの処理
                         Intent map1_change = new Intent(MapActivity.this, map1Activity.class);
-                        map1_change.putExtra("kind_game",1);
+                        map1_change.putExtra("kind_game", 1);
                         startActivity(map1_change);//画面遷移を行う
                     }
                 });
@@ -47,7 +74,7 @@ public class MapActivity extends AppCompatActivity {
             @Override
             public void onClick(final View v) {//押下時の処理
                 //確認ダイアログの作成
-                AlertDialog.Builder alertDialog = new AlertDialog.Builder(MapActivity.this,R.style.MyAlertDialogStyle);
+                AlertDialog.Builder alertDialog = new AlertDialog.Builder(MapActivity.this, R.style.MyAlertDialogStyle);
                 alertDialog.setTitle("和紙(仮)");
                 alertDialog.setMessage("このあそびをやってみる？？");
                 alertDialog.setPositiveButton("　　　 すすむ　　　 ", new DialogInterface.OnClickListener() {
@@ -56,7 +83,7 @@ public class MapActivity extends AppCompatActivity {
 
                         // TODO:すすむボタンが押されたときの処理
                         Intent map2_change = new Intent(MapActivity.this, map2Activity.class);
-                        map2_change.putExtra("kind_game",2);
+                        map2_change.putExtra("kind_game", 2);
                         startActivity(map2_change);//画面遷移を行う
                     }
                 });
@@ -69,7 +96,7 @@ public class MapActivity extends AppCompatActivity {
             @Override
             public void onClick(final View v) {//押下時の処理
                 //確認ダイアログの作成
-                AlertDialog.Builder alertDialog = new AlertDialog.Builder(MapActivity.this,R.style.MyAlertDialogStyle);
+                AlertDialog.Builder alertDialog = new AlertDialog.Builder(MapActivity.this, R.style.MyAlertDialogStyle);
                 alertDialog.setTitle("水飴(仮)");
                 alertDialog.setMessage("このあそびをやってみる？？");
                 alertDialog.setPositiveButton("　　　 すすむ　　　 ", new DialogInterface.OnClickListener() {
@@ -78,7 +105,7 @@ public class MapActivity extends AppCompatActivity {
 
                         // TODO:すすむボタンが押されたときの処理
                         Intent map3_change = new Intent(MapActivity.this, map3Activity.class);
-                        map3_change.putExtra("kind_game",3);
+                        map3_change.putExtra("kind_game", 3);
                         startActivity(map3_change);//画面遷移を行う
                     }
                 });
@@ -91,7 +118,7 @@ public class MapActivity extends AppCompatActivity {
             @Override
             public void onClick(final View v) {//押下時の処理
                 //確認ダイアログの作成
-                AlertDialog.Builder alertDialog = new AlertDialog.Builder(MapActivity.this,R.style.MyAlertDialogStyle);
+                AlertDialog.Builder alertDialog = new AlertDialog.Builder(MapActivity.this, R.style.MyAlertDialogStyle);
                 alertDialog.setTitle("きんつば(仮)");
                 alertDialog.setMessage("このあそびをやってみる？？");
                 alertDialog.setPositiveButton("　　　 すすむ　　　 ", new DialogInterface.OnClickListener() {
@@ -100,7 +127,7 @@ public class MapActivity extends AppCompatActivity {
 
                         // TODO:すすむボタンが押されたときの処理
                         Intent map4_change = new Intent(MapActivity.this, map4Activity.class);
-                        map4_change.putExtra("kind_game",4);
+                        map4_change.putExtra("kind_game", 4);
                         startActivity(map4_change);//画面遷移を行う
                     }
                 });
@@ -113,7 +140,7 @@ public class MapActivity extends AppCompatActivity {
             @Override
             public void onClick(final View v) {//押下時の処理
                 //確認ダイアログの作成
-                AlertDialog.Builder alertDialog = new AlertDialog.Builder(MapActivity.this,R.style.MyAlertDialogStyle);
+                AlertDialog.Builder alertDialog = new AlertDialog.Builder(MapActivity.this, R.style.MyAlertDialogStyle);
                 alertDialog.setTitle("野菜クイズ(仮)");
                 alertDialog.setMessage("このあそびをやってみる？？");
                 alertDialog.setPositiveButton("　　　 すすむ　　　 ", new DialogInterface.OnClickListener() {
@@ -122,7 +149,7 @@ public class MapActivity extends AppCompatActivity {
 
                         // TODO:すすむボタンが押されたときの処理
                         Intent map5_change = new Intent(MapActivity.this, map5Activity.class);
-                        map5_change.putExtra("kind_game",5);
+                        map5_change.putExtra("kind_game", 5);
                         startActivity(map5_change);//画面遷移を行う
                     }
                 });
@@ -135,7 +162,7 @@ public class MapActivity extends AppCompatActivity {
             @Override
             public void onClick(final View v) {//押下時の処理
                 //確認ダイアログの作成
-                AlertDialog.Builder alertDialog = new AlertDialog.Builder(MapActivity.this,R.style.MyAlertDialogStyle);
+                AlertDialog.Builder alertDialog = new AlertDialog.Builder(MapActivity.this, R.style.MyAlertDialogStyle);
                 alertDialog.setTitle("金箔(仮)");
                 alertDialog.setMessage("このあそびをやってみる？？");
                 alertDialog.setPositiveButton("　　　 すすむ　　　 ", new DialogInterface.OnClickListener() {
@@ -144,7 +171,7 @@ public class MapActivity extends AppCompatActivity {
 
                         // TODO:すすむボタンが押されたときの処理
                         Intent map6_change = new Intent(MapActivity.this, map6Activity.class);
-                        map6_change.putExtra("kind_game",6);
+                        map6_change.putExtra("kind_game", 6);
                         startActivity(map6_change);//画面遷移を行う
                     }
                 });
@@ -157,7 +184,7 @@ public class MapActivity extends AppCompatActivity {
             @Override
             public void onClick(final View v) {//押下時の処理
                 //確認ダイアログの作成
-                AlertDialog.Builder alertDialog = new AlertDialog.Builder(MapActivity.this,R.style.MyAlertDialogStyle);
+                AlertDialog.Builder alertDialog = new AlertDialog.Builder(MapActivity.this, R.style.MyAlertDialogStyle);
                 alertDialog.setTitle("漆器(仮)");
                 alertDialog.setMessage("このあそびをやってみる？？");
                 alertDialog.setPositiveButton("　　　 すすむ　　　 ", new DialogInterface.OnClickListener() {
@@ -166,7 +193,7 @@ public class MapActivity extends AppCompatActivity {
 
                         // TODO:すすむボタンが押されたときの処理
                         Intent map7_change = new Intent(MapActivity.this, map7Activity.class);
-                        map7_change.putExtra("kind_game",7);
+                        map7_change.putExtra("kind_game", 7);
                         startActivity(map7_change);//画面遷移を行う
                     }
                 });
@@ -192,14 +219,14 @@ public class MapActivity extends AppCompatActivity {
         };
 
 /*map1へ画面遷移*/
-        final Button map1_1 =(Button)findViewById(R.id.button1_1);
-        final Button map1_2 =(Button)findViewById(R.id.button1_2);
-        final Button map1_3 =(Button)findViewById(R.id.button1_3);
-        final Button map1_4 =(Button)findViewById(R.id.button1_4);
-        final Button map1_5 =(Button)findViewById(R.id.button1_5);
-        final Button map1_6 =(Button)findViewById(R.id.button1_6);
-        final Button map1_7 =(Button)findViewById(R.id.button1_7);
-        final Button map1_8 =(Button)findViewById(R.id.button1_8);
+        final Button map1_1 = (Button) findViewById(R.id.button1_1);
+        final Button map1_2 = (Button) findViewById(R.id.button1_2);
+        final Button map1_3 = (Button) findViewById(R.id.button1_3);
+        final Button map1_4 = (Button) findViewById(R.id.button1_4);
+        final Button map1_5 = (Button) findViewById(R.id.button1_5);
+        final Button map1_6 = (Button) findViewById(R.id.button1_6);
+        final Button map1_7 = (Button) findViewById(R.id.button1_7);
+        final Button map1_8 = (Button) findViewById(R.id.button1_8);
 
         map1_1.setOnClickListener(a);
         map1_2.setOnClickListener(a);
@@ -211,11 +238,11 @@ public class MapActivity extends AppCompatActivity {
         map1_8.setOnClickListener(a);
 
 /*map2へ画面遷移*/
-        final Button map2_1 =(Button)findViewById(R.id.button2_1);
-        final Button map2_2 =(Button)findViewById(R.id.button2_2);
-        final Button map2_3 =(Button)findViewById(R.id.button2_3);
-        final Button map2_4 =(Button)findViewById(R.id.button2_4);
-        final Button map2_5 =(Button)findViewById(R.id.button2_5);
+        final Button map2_1 = (Button) findViewById(R.id.button2_1);
+        final Button map2_2 = (Button) findViewById(R.id.button2_2);
+        final Button map2_3 = (Button) findViewById(R.id.button2_3);
+        final Button map2_4 = (Button) findViewById(R.id.button2_4);
+        final Button map2_5 = (Button) findViewById(R.id.button2_5);
 
         map2_1.setOnClickListener(b);
         map2_2.setOnClickListener(b);
@@ -225,11 +252,11 @@ public class MapActivity extends AppCompatActivity {
 
 /*map3へ画面遷移*/
 
-        final Button map3_1 = (Button)findViewById(R.id.button3_1);
-        final Button map3_2 = (Button)findViewById(R.id.button3_2);
-        final Button map3_3 = (Button)findViewById(R.id.button3_3);
-        final Button map3_4 = (Button)findViewById(R.id.button3_4);
-        final Button map3_5 = (Button)findViewById(R.id.button3_5);
+        final Button map3_1 = (Button) findViewById(R.id.button3_1);
+        final Button map3_2 = (Button) findViewById(R.id.button3_2);
+        final Button map3_3 = (Button) findViewById(R.id.button3_3);
+        final Button map3_4 = (Button) findViewById(R.id.button3_4);
+        final Button map3_5 = (Button) findViewById(R.id.button3_5);
         map3_1.setOnClickListener(c);
         map3_2.setOnClickListener(c);
         map3_3.setOnClickListener(c);
@@ -237,19 +264,19 @@ public class MapActivity extends AppCompatActivity {
         map3_5.setOnClickListener(c);
 
 /*map4へ画面遷移*/
-        final Button map4_1 = (Button)findViewById(R.id.button4_1);
-        final Button map4_2 = (Button)findViewById(R.id.button4_2);
-        final Button map4_3 = (Button)findViewById(R.id.button4_3);
+        final Button map4_1 = (Button) findViewById(R.id.button4_1);
+        final Button map4_2 = (Button) findViewById(R.id.button4_2);
+        final Button map4_3 = (Button) findViewById(R.id.button4_3);
 
         map4_1.setOnClickListener(d);
         map4_2.setOnClickListener(d);
         map4_3.setOnClickListener(d);
 
 /*map5へ画面遷移*/
-        final Button map5_1 = (Button)findViewById(R.id.button5_1);
-        final Button map5_2 = (Button)findViewById(R.id.button5_2);
-        final Button map5_3 = (Button)findViewById(R.id.button5_3);
-        final Button map5_4 = (Button)findViewById(R.id.button5_4);
+        final Button map5_1 = (Button) findViewById(R.id.button5_1);
+        final Button map5_2 = (Button) findViewById(R.id.button5_2);
+        final Button map5_3 = (Button) findViewById(R.id.button5_3);
+        final Button map5_4 = (Button) findViewById(R.id.button5_4);
 
         map5_1.setOnClickListener(e);
         map5_2.setOnClickListener(e);
@@ -257,10 +284,10 @@ public class MapActivity extends AppCompatActivity {
         map5_4.setOnClickListener(e);
 
 /*map6へ画面遷移*/
-        final Button map6_1 = (Button)findViewById(R.id.button6_1);
-        final Button map6_2 = (Button)findViewById(R.id.button6_2);
-        final Button map6_3 = (Button)findViewById(R.id.button6_3);
-        final Button map6_4 = (Button)findViewById(R.id.button6_4);
+        final Button map6_1 = (Button) findViewById(R.id.button6_1);
+        final Button map6_2 = (Button) findViewById(R.id.button6_2);
+        final Button map6_3 = (Button) findViewById(R.id.button6_3);
+        final Button map6_4 = (Button) findViewById(R.id.button6_4);
 
         map6_1.setOnClickListener(f);
         map6_2.setOnClickListener(f);
@@ -268,10 +295,10 @@ public class MapActivity extends AppCompatActivity {
         map6_4.setOnClickListener(f);
 
 /*map7へ画面遷移*/
-        final Button map7_1 = (Button)findViewById(R.id.button7_1);
-        final Button map7_2 = (Button)findViewById(R.id.button7_2);
-        final Button map7_3 = (Button)findViewById(R.id.button7_3);
-        final Button map7_4 = (Button)findViewById(R.id.button7_4);
+        final Button map7_1 = (Button) findViewById(R.id.button7_1);
+        final Button map7_2 = (Button) findViewById(R.id.button7_2);
+        final Button map7_3 = (Button) findViewById(R.id.button7_3);
+        final Button map7_4 = (Button) findViewById(R.id.button7_4);
 
         map7_1.setOnClickListener(g);
         map7_2.setOnClickListener(g);
@@ -279,15 +306,14 @@ public class MapActivity extends AppCompatActivity {
         map7_4.setOnClickListener(g);
 
 /*図鑑へ画面遷移*/
-        final Button zukan = (Button)findViewById(R.id.zukan);
+        final Button zukan = (Button) findViewById(R.id.zukan);
 
         zukan.setOnClickListener(z);
 
 /*イベントへ画面遷移*/
-        final Button event = (Button)findViewById(R.id.event);
+        final Button event = (Button) findViewById(R.id.event);
 
         event.setOnClickListener(i);
 
     }
-
 }
--- app/src/main/java/smartphoneapp_project/kanazawaapp_2017/Washi3Activity.java
+++ app/src/main/java/smartphoneapp_project/kanazawaapp_2017/Washi3Activity.java
@@ -1,28 +1,43 @@
 package smartphoneapp_project.kanazawaapp_2017;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by isidadaiti on 2018/01/03.
  */
 
 public class Washi3Activity extends Activity {
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_washi3);
-    }
-    public void onclicknextbutton(View v){
-        Intent intent= new Intent(this,MapActivity.class);
-        startActivity(intent);
+        Button tomap =(Button)findViewById(R.id.button2);
+        tomap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // "addKey"という名前でインスタンスを生成
+                SharedPreferences data = getSharedPreferences("addKey", Context.MODE_PRIVATE);
 
+                //データの保存
+                SharedPreferences.Editor editor = data.edit();//オブジェクト取得
+                editor.putString("StatusSave", "WashiEasy");//キー：StatusSava 値：WashiEasy
+                editor.apply();//データの保存
+
+                Intent intent= new Intent(Washi3Activity.this,MapActivity.class);
+                startActivity(intent);
+            }
+        });
     }
-    public void onclickreturnbutton(View view){
-        Intent intent= new Intent(this,DifficultyActivity.class);
-        startActivity(intent);
-    }
+
+//        Intent intent= new Intent(this,DifficultyActivity.class);
+//        startActivity(intent);
+
 }
