bfa4b8046e840e3b3587fcd89b15ba8c6bb7c5d6
shun6934 <>
2018-02-07 14:39:22 +0900

    回転に必要な関数追加

---
+++ app/src/main/java/smartphoneapp_project/kanazawaapp_2017/RotationGestureDetector.java
@@ -0,0 +1,51 @@
+package smartphoneapp_project.kanazawaapp_2017;
+
+import android.support.annotation.NonNull;
+import android.view.MotionEvent;
+
+public class RotationGestureDetector {
+
+    interface OnRotationGestureListener {
+        void onRotate(float angle);
+    }
+
+    private float centerX = 0;
+    private float centerY = 0;
+    public float angle = 0;
+
+    private OnRotationGestureListener listener;
+
+    public RotationGestureDetector(@NonNull OnRotationGestureListener listener) {
+        this.listener = listener;
+    }
+
+    public void setCenterX(float centerX) {
+        this.centerX = centerX;
+    }
+
+    public void setCenterY(float centerY) {
+        this.centerY = centerY;
+    }
+
+    public void onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_MOVE) {
+            float angle = calcAngle(event.getX(), event.getY(), centerX, centerY);
+            listener.onRotate(angle);
+        } else if (event.getAction() == MotionEvent.ACTION_UP) {
+            angle = 0;
+        }
+    }
+
+    private float calcAngle(float fingerX, float fingerY, float centerX, float centerY) {
+        float x = fingerX - centerX;
+        float y = fingerY - centerY;
+        float theta = (float) Math.atan2(y, x);
+        float angle = (float) Math.toDegrees(theta);
+
+        if (angle < 0) angle += 360;
+        angle = Math.abs(360 - angle);
+        if (angle == 360) angle = 0.0F;
+
+        return angle;
+    }
+}
